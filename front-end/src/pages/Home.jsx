b582250f7060e9f36f4ed802be6b37644bdc3c69
Eric Bwire <ericleobwire@gmail.com>
2025-06-08 16:22:59 +0300

    PUBLIC POSTS
    - Limited public posts visibke to 3 when user is not logged in

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -56,7 +56,7 @@ export default function Home() {
         const sortedPosts = res.data.data.sort(
           (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
         );
-        setPublicPosts(sortedPosts);
+        setPublicPosts(user ? sortedPosts : sortedPosts.slice(0, 3));
       } catch (error) {
         console.error("Failed to fetch public posts:", error);
       } finally {
