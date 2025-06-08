3a5b6c12bd25941664ef54aceb539cd77d29dcae
Eric Bwire <ericleobwire@gmail.com>
2025-06-08 16:15:38 +0300

    Mobile View & Public Posts
    - Added some styling for mobile devices
    - Limited public posts visibke to 3 when user is not logged in

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -56,7 +56,8 @@ export default function Home() {
         const sortedPosts = res.data.data.sort(
           (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
         );
-        setPublicPosts(sortedPosts);
+        // If user is not logged in, only show 3 posts
+        setPublicPosts(user ? sortedPosts : sortedPosts.slice(0, 3));
       } catch (error) {
         console.error("Failed to fetch public posts:", error);
       } finally {
@@ -65,7 +66,7 @@ export default function Home() {
     };
 
     fetchPublicPosts();
-  }, []);
+  }, [user]);
 
   // Fetch user's posts
   useEffect(() => {
